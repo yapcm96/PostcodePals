e5fab293963da5280e1bfbf69cecd541f754e376
beth <beth.gooding@hotmail.com>
2021-04-01 11:06:05 +0100

    Sets up database and Task model

--- project/neighbourhood_heroes/pages/models.py
+++ project/neighbourhood_heroes/pages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    task = models.CharField(max_length=200, null = True)
+    type_of_task = models.CharField(max_length=200, null = True)
+    location = models.CharField(max_length=200, null = True)
+    estimated_duration_mins = models.DecimalField(decimal_places = 2, max_digits = 10)
+    deadline = models.CharField(max_length=200, null = True)
+    notes = models.CharField(max_length=200, null = True)
+    status = models.CharField(max_length=200, default = "Open")
+    task_setter = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return f'{self.task} for {self.task_setter}'
+    
