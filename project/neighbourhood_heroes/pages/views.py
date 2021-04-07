b4b9709ea134ea91edb6accbd123f48d69f324f4
beth <beth.gooding@hotmail.com>
2021-04-07 19:56:38 +0100

    Imports Http404 in views.py

--- project/neighbourhood_heroes/pages/views.py
+++ project/neighbourhood_heroes/pages/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
