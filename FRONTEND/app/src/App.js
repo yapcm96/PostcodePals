a14c50ee13247e3d28f39ba30df14ea69e1cd5b4
IssyYeung <issyyeung@hotmail.co.uk>
2021-04-19 19:10:14 +0100

    Removed unneeded browser router from app

--- FRONTEND/app/src/App.js
+++ FRONTEND/app/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import HomePage from "./pages/HomePage";
@@ -8,17 +8,15 @@ import VolunteerPage from "./pages/VolunteerPage";
 function App() {
   //
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/volunteer" component={VolunteerPage} />
-          <Route exact path="/asker" component={AskerPage} />
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <Header />
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/volunteer" component={VolunteerPage} />
+        <Route exact path="/asker" component={AskerPage} />
+      </Switch>
+    </div>
   );
 }
 
