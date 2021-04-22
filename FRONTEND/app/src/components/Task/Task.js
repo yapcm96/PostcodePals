ec7809bdda3668d7a56e92a10b6021aa6d197100
Choon Meng (Johnny) Yap <yapcm96@hotmail.com>
2021-04-22 17:54:58 +0100

    Styled tasks according to status, truncated long task name

--- FRONTEND/app/src/components/Task/Task.js
+++ FRONTEND/app/src/components/Task/Task.js
@@ -7,19 +7,30 @@ import { GoLocation } from "react-icons/go";
 
 const Task = ({ taskItem }) => {
   const taskURL = `/tasks/${taskItem.id}`;
-  // console.log(taskItem.id)
 
   return (
     <Link to={taskURL} className={style.task}>
-      <h3>
-        <BiTask /> {taskItem.task}
-      </h3>
-      <h4>
-        <GoLocation /> {taskItem.location}
-      </h4>
-      <h4>
-        <BiTimeFive /> {taskItem.deadline}
-      </h4>
+      <div className={style.summary}>
+        <h3>
+          <BiTask /> {taskItem.task}
+        </h3>
+        <h4>
+          <GoLocation /> {taskItem.location}
+        </h4>
+        <h4>
+          <BiTimeFive /> {taskItem.deadline}
+        </h4>
+      </div>
+
+      <div className={
+        taskItem.completed ? style.completed :
+        taskItem.assigned ? style.assigned : style.unassigned
+      }>
+        
+        {taskItem.completed ? <h5>Completed</h5> : <>
+        {taskItem.assigned ? <h5>Assigned</h5> : <h5>Unassigned</h5>} </>}
+      </div>
+
     </Link>
   );
 };
